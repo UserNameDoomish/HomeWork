84f9aa7a9a25bcc402762722c60f44e82b2b6bb6
UserNameDoomish <125404351+UserNameDoomish@users.noreply.github.com>
2023-02-23 22:25:31 +0200

    HomeWorkJava1

---
+++ HomeWork/src/Home/Work/HomeClass.java
@@ -0,0 +1,9 @@
+package Home.Work;
+
+public class HomeClass {
+    public static void main(String[]args){
+        System.out.println ("Hello i am HomeClass");
+    }
+
+
+}
---
+++ HomeWork/src/I/AM/IAm.java
@@ -0,0 +1,7 @@
+package I.AM;
+
+public class IAm {
+    public static void main(String[]args){
+        System.out.println ("Hello i am I.AM");
+    }
+}
