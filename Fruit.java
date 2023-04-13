a1d7ba35aa670ec4028b6e47fa92d0e7830edcaa
UserNameDoomish <125404351+UserNameDoomish@users.noreply.github.com>
2023-04-13 11:59:40 +0300

    JavaHomeWork8

---
+++ Fruit.java
@@ -0,0 +1,16 @@
+
+    public class Fruit {
+        String name;
+        boolean isTasty;
+        String color;
+
+        // конструктор класса
+        public Fruit(String name, boolean isTasty, String color) {
+            this.name = name;
+            this.isTasty = isTasty;
+            this.color = color;
+        }
+    }
+
+
+
